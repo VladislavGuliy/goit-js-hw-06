d35449575caf901c63329a21df5a500a79b23e2e
VladislavGuliy <vladgulij777@gmail.com>
2023-05-10 16:44:25 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,16 +1,12 @@
 const numberOfCategories = (categories) => {
-    console.log(`Number of categories: ${categories.length}`);
+  console.log(`Number of categories: ${categories.length}`);
+  console.log("");
+  categories.forEach((category) => {
+    console.log(`Category: ${category.firstElementChild.textContent}`);
+    console.log(`Elements: ${category.lastElementChild.children.length}`);
     console.log("");
-    categories.forEach((category) => {
-      const nameOfCategoryRef = category.querySelector("h2");
-      const elementsRef = category.querySelectorAll("li");
-      console.log(`Category: ${nameOfCategoryRef.textContent}`);
-      console.log(`Elements: ${elementsRef.length}`);
-      console.log("");
-    });
-  };
-  
-  const listWithCategoriesRef = document.querySelectorAll(".item");
-  
-  numberOfCategories(listWithCategoriesRef);
-  
+  });
+};
+
+const listWithCategoriesRef = document.querySelectorAll(".item");
+numberOfCategories(listWithCategoriesRef);
