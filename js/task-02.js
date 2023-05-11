81dbddaa13e15e64a95c396f2ce94757f8cb0816
VladislavGuliy <vladgulij777@gmail.com>
2023-05-11 17:08:00 +0300

    Рішення завдання №2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,23 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const listEl = document.querySelector("#ingredients");
+
+const createElements = (items) => {
+  const arrayItems = [];
+  items.forEach((item) => {
+    const linkEl = document.createElement("li");
+    linkEl.classList.add("item");
+    linkEl.textContent = item;
+    arrayItems.push(linkEl);
+  });
+
+  return arrayItems;
+};
+
+listEl.append(...createElements(ingredients));
