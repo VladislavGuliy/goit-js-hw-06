91b7d0e4ef34d4ab0e2a9b5b1c59ace7d1388def
VladislavGuliy <vladgulij777@gmail.com>
2023-05-14 21:28:54 +0300

    Рішення завдання №4

---
+++ js/task-04.js
@@ -0,0 +1,19 @@
+const buttons = document.querySelectorAll("#counter button");
+function handleDecrementIncrementClick() {
+  const action = this.dataset.action;
+  const value = this.parentElement.querySelector("#value");
+  const counterValue = Number(value.textContent);
+  let newValue;
+
+  if (action === "increment") {
+    newValue = counterValue + 1;
+  } else {
+    newValue = counterValue - 1;
+  }
+  value.textContent = newValue;
+  console.log(counterValue);
+}
+
+buttons.forEach((button) => {
+  button.addEventListener("click", handleDecrementIncrementClick);
+});
