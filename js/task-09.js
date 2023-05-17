91838878c25823f29b262bf42eeaa27219e20970
VladislavGuliy <vladgulij777@gmail.com>
2023-05-17 23:53:22 +0300

    Рішення завдання №9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonRef = document.querySelector(".change-color");
+const spanRef = document.querySelector(".color");
+const bodyRef = document.querySelector("body");
+
+buttonRef.addEventListener("click", onButtonChangeColorClick);
+
+function onButtonChangeColorClick(event) {
+  const colorChange = getRandomHexColor();
+
+  spanRef.textContent = colorChange;
+  bodyRef.style.backgroundColor = colorChange;
+}
